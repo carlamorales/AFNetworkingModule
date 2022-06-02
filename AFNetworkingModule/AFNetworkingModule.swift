d8adf728be626bfc34051d7271aa6e570f0123aa
carlamorales <carlamoralespacheco@gmail.com>
2022-06-02 16:00:53 -0400

    método genérico

--- AFNetworkingModule/AFNetworkingModule.swift
+++ AFNetworkingModule/AFNetworkingModule.swift
@@ -1,7 +1,13 @@
 import Alamofire
 
 public class AFNetworkingModule {
-    public func networkingModule() {
-        
+    public func networkingModule<T1: Decodable, T2: Error>(url: String, onCompletion: @escaping (Result<[T1], T2>) -> Void) {
+        AF.request(url, method: .get).validate(statusCode: 200...299).responseDecodable(of: T1.self) { response in
+            if let list = response.value {
+                onCompletion(.success([list]))
+            } else {
+                onCompletion(.failure(T2.self as! T2))
+            }
+        }
     }
 }
