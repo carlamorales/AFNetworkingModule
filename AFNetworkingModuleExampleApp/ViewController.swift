8d2210f1fa96ed9bd08b93ce2db278d33b67fc97
carlamorales <carlamoralespacheco@gmail.com>
2022-06-03 10:04:57 -0400

    se corrige warning

--- AFNetworkingModuleExampleApp/ViewController.swift
+++ AFNetworkingModuleExampleApp/ViewController.swift
@@ -1,11 +1,43 @@
 import UIKit
 import AFNetworkingModule
 
+struct Character: Decodable {
+    let identifier: Int
+    let name: String
+    let status: String
+    let species: String
+    let image: String
+    
+    enum CodingKeys: String, CodingKey {
+        case identifier = "id"
+        case name
+        case status
+        case species
+        case image
+    }
+}
+
+struct CharactersList: Decodable {
+    let results: [Character]
+}
+
+struct ApiCallError: Error {
+    let message: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        AFNetworkingModule().networkingModule(url: "https://rickandmortyapi.com/api/character") { (result: Result<CharactersList, ApiCallError>) in
+            switch result {
+            case .success(let list):
+                print(list.results)
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 
 }
